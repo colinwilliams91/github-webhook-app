77e2a0c83608a2a80ec7fbca9c0b2635bc5b3fa0
Colin Williams <colin.williams.dev@gmail.com>
2024-02-07 21:09:55 -0600

    chore: local vars from env, config app client obj

--- app.js
+++ app.js
@@ -4,3 +4,20 @@ import { createNodeMiddleware } from "octokit";
 import fs from "fs";
 import http from "http";
 
+dotenv.config();
+
+
+const appId = process.env.APP_ID;
+const webHookSecret = process.env.WEBHOOK_SECRET;
+const privateKeyPath = process.env.PRIVATE_KEY_PATH;
+
+const privateKey = fs.readFileSync(privateKeyPath, "utf-8");
+
+const app = new App({
+  appId: appId,
+  privateKey: privateKey,
+  webhooks: {
+    secret: webHookSecret
+  },
+});
+
